f5bda69af1d26045dd216a0b33b72ac3e6c13c50
VIC <vic868@126.com>
2019-05-21 17:24:00 +0800

    es

--- spingsvr/src/main/java/com/SpingsvrApplication.java
+++ spingsvr/src/main/java/com/SpingsvrApplication.java
@@ -1,15 +1,19 @@
 package com;
 
-import org.springframework.boot.SpringApplication;
+import javafx.application.Application;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class SpingsvrApplication {
 
+
+
     public static void main(String[] args) {
-        SpringApplication.run(SpingsvrApplication.class, args);
+            new SpringApplicationBuilder(SpingsvrApplication.class).web(WebApplicationType.SERVLET).run(args);
     }
 
 }
--- spingsvr/src/main/java/com/controller/ConsulApp.java
+++ spingsvr/src/main/java/com/controller/ConsulApp.java
@@ -6,6 +6,8 @@ package com.controller;
  * @author: vic
  * @create: 2019-05-20 16:49
  **/
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConsulApp {
+    @Value("${server.port}")
+    private String port;
 
     @RequestMapping("/home")
     public Object home() {
+
+        System.out.println(port+"------");
         System.out.println("1111111111111");
         return "OK11";
     }
